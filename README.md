652ca5d080c9d2ebbdf1ee9276ebd09218334f54
Awilum <awilum@msn.com>
2015-12-08 00:03:14 +0300

    Init Commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# morfy-plugin-snipplr
+# Snipplr
+
 Plugin for quick embedding Snipplr snippets.
+
+## Installation
+See [this instruction](http://morfy.org/documentation/plugins/plugins-installation)
+
+## Usage in page content
+
+```
+{snipplr id=205}
+```
+
+## Usage in template
+
+```smarty
+{snipplr(205)}
+```
+
+## Options
+
+| name  | value | description |
+|---|---|---|
+| enabled | true | or `false` to disable the plugin |
+
+## License
+See  [LICENSE](https://github.com/morfy-cms/morfy-plugin-snipplr/blob/master/LICENSE)
---
+++ snipplr.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Morfy Snipplr Plugin
+ *
+ * (c) Romanenko Sergey / Awilum <awilum@msn.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// Shortcode: {snipplr}
+Shortcode::add('snipplr', function ($attributes) {
+
+    // Extract
+    extract($attributes);
+
+    return (isset($id)) ? snipplr($id) : '';
+});
+
+/**
+ * Return Snipplr snippet
+ *
+ *  <code>
+ *      snipplr(12345);
+ *  </code>
+ *
+ * @param  int    $id Snipplr ID
+ * @return string
+ */
+function snipplr($id)
+{
+    return (isset($id)) ? "<div id=\"snipplr_embed_{$id}\" class=\"snipplr_embed\"><script type=\"text/javascript\" src=\"http://snipplr.com/js/embed.js\"></script><script type=\"text/javascript\" src=\"http://snipplr.com/json/{$id}\"></script>" : '';
+}
